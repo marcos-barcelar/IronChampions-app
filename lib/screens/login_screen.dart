9125d5bc8e0dd880afbdf6a080eff525df150a5d
MARCOS GIOVANE DE OLIVEIRA BARCELAR <marcos.5988@aluno.pr.senac.br>
2024-03-21 16:46:29 -0300

    Tela de login quase finalizada

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,14 +9,104 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: Color(0xFF1C1C1C),
       body: Center(
         child: Container(
-          width: 300,
+          width: 400,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset(
                 'assets/Logo_IRONCHAMPIONSgym.png',
-                height: 75,
+                height: 120,
               ),
-
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Color(0xFF870B00)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Color(0xFF870B00)),
+                    ),
+                  ),
+                  cursorColor: Color(0xFF870B00),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Color(0xFF870B00)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Color(0xFF870B00)),
+                    ),
+                  ),
+                  cursorColor: Color(0xFF870B00),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF870B00),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                ),
+                child: Container(
+                  width: 150,
+                  alignment: Alignment.center,
+                  child: Text('Entrar'),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Não tem conta? Crie uma aqui',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Esqueci a senha',
+                        style: TextStyle(
+                            color: Color(0xFF870B00),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
