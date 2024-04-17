539cb3b4effce7f289dee9a320767ab898dc807a
MARCOS GIOVANE DE OLIVEIRA BARCELAR <marcos.5988@aluno.pr.senac.br>
2024-04-17 14:41:42 -0300

    tela principal refeita e finalizada

--- lib/screens/primary_screen.dart
+++ lib/screens/primary_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-class PrimaryScreen extends StatelessWidget {
+class PrimaryScreen extends StatefulWidget {
   const PrimaryScreen({Key? key}) : super(key: key);
 
+  @override
+  _PrimaryScreenState createState() => _PrimaryScreenState();
+}
+
+class _PrimaryScreenState extends State<PrimaryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1C1C1C),
       appBar: AppBar(
         backgroundColor: Color(0xFF1C1C1C),
+        toolbarHeight: 60,
+        elevation: 0,
         actions: [
           IconButton(
             onPressed: () {},
@@ -17,66 +24,168 @@ class PrimaryScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Fichas de treinos',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Olá, Aluno',
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Fichas de treinos',
                     style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 24,
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: ExpansionTile(
-                    collapsedBackgroundColor: Colors.black,
-                    title: Text(
-                      'Treino 1',
-                      style: TextStyle(
-                        color: Colors.white,
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Text(
+                        'Olá, Aluno',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                    trailing: Icon(
-                      Icons.expand_more,
-                      color: Colors.white,
+                  ),
+                  Theme(
+                    data: Theme.of(context).copyWith(
+                      canvasColor: Colors.black,
                     ),
-                    backgroundColor: Color(0xFF1C1C1C),
-                    children: <Widget>[
-                      ListTile(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ExpansionTile(
+                        collapsedBackgroundColor: Colors.black,
+                        backgroundColor: Colors.black,
+                        iconColor: Colors.white,
+                        collapsedIconColor: Colors.white,
                         title: Text(
-                          'This is tile number 1',
+                          'Treino 1',
                           style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
                         ),
-
+                        children: [
+                          ListTile(
+                            title: Text(
+                              'Segunda',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              'Treino de Perna',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '1. Exercício A',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '2. Exercício B',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '3. Exercício C',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              'Terça',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              'Treino de Peito',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '1. Exercício A',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '2. Exercício B',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '3. Exercício C',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        backgroundColor: Color(0xFF870B00),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
